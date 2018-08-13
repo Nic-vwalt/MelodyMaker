e9110fb0a6ef59fa1f667dd26b401bd85aa33477
NICOLAS-LAPTOP\Nicolas <nicolas.vwalt@wyobi.com>
2018-08-13 19:59:44 +0200

    update

---
+++ missing_data.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('Die CSV kom hier')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 3].values
+
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
